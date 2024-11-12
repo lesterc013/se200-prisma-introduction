3b776cc42658273354354d12898aca43b888bc87
lesterchan <lesterc013@gmail.com>
2024-11-12 21:20:30 +0800

    Read all rows from db and return them as divs

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+// Read db data to extract the rows
+import { db } from "../db";
+
+export default async function Page() {
+    const expenseItems = await db.expenseItem.findMany();
+
+    return (
+        <>
+            <div>
+                {expenseItems.map((expenseItem) => (
+                    <div id={`${expenseItem.id}`}>{expenseItem.item}</div>
+                ))}
+            </div>
+        </>
+    );
 }
